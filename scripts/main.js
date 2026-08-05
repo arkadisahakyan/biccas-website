9907968f23816d28a66a54e759828aba11e56153
Arkadi Sahakyan <arkadijsaakyan3@gmail.com>
2026-08-05 10:15:55 +0300

    Initial commit. Animated hero section

---
+++ scripts/main.js
@@ -0,0 +1,174 @@
+gsap.registerPlugin(MotionPathPlugin);
+gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(SplitText);
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  initializeAnimatedHamburgerMenu();
+
+  const tl = gsap.timeline();
+
+  // animate the logo
+  const logo = document.querySelector(".hero-section__logo");
+  const logoChars = SplitText.create(logo, {
+    type: "chars",
+    autoSplit: true,
+    onSplit: (self) => {
+      return tl.from(self.chars, {
+        duration: 0.5,
+        x: logo.clientWidth,
+        opacity: 0,
+        stagger: 0.15,
+        ease: "power1.in",
+      });
+    },
+  });
+
+  // animate the curved line
+  tl.fromTo(
+    ".curved-line path",
+    { drawSVG: "0" },
+    {
+      duration: 0.8,
+      drawSVG: "100%",
+      ease: "power2.inOut",
+      delay: 0.3,
+    },
+  );
+
+  // animate the try for free button
+  tl.to(".try-for-free", {
+    scale: 1.1,
+    duration: 0.2,
+    delay: 0.3,
+    repeat: 1,
+    yoyo: true,
+    ease: "power1.out",
+  });
+
+  // animate marquee block
+  const marqueeInstance = document.querySelector(".hero-section__partners");
+  const marqueeOriginalPart = marqueeInstance.children[0];
+  const marqueeClonedPart = marqueeOriginalPart.cloneNode(true);
+
+  marqueeInstance.appendChild(marqueeClonedPart);
+  gsap.to([marqueeOriginalPart, marqueeClonedPart], {
+    duration: 15,
+    xPercent: -100,
+    repeat: -1,
+    ease: "none",
+  });
+});
+
+function initializeAnimatedHamburgerMenu() {
+  const toggleButton = document.querySelector(".hero-section__menu-toggle svg");
+  const hamburgerButton = new HamburgerButton(toggleButton);
+  const buttonAnimator = new HamburgerButtonAnimator(hamburgerButton);
+
+  const hamburgerMenu = new HamburgerMenu(
+    document.querySelector(".hero-section__menu"),
+    document.querySelector(".hero-section__nav"),
+    hamburgerButton,
+  );
+  const menuAnimator = new HamburgerMenuAnimator(hamburgerMenu);
+
+  toggleButton.addEventListener("click", (event) => {
+    hamburgerButton.isOpen = !hamburgerButton.isOpen;
+    buttonAnimator.animateButtonToggle();
+    menuAnimator.animateHamburgerMenu();
+  });
+
+  window.addEventListener("resize", () => {
+    menuAnimator.clearProps();
+  });
+}
+
+class HamburgerButton {
+  constructor(buttonInstance) {
+    this.buttonInstance = buttonInstance;
+    this.topLine = buttonInstance.children[0];
+    this.middleLine = buttonInstance.children[1];
+    this.bottomLine = buttonInstance.children[2];
+    this.isOpen = false;
+  }
+}
+
+class HamburgerButtonAnimator {
+  static yOffset = 5;
+  static buttonToggleDuration = 0.2;
+
+  constructor(hamburgerButton) {
+    this.hamburgerButton = hamburgerButton;
+  }
+
+  animateButtonToggle() {
+    gsap.to(this.hamburgerButton.topLine, {
+      duration: HamburgerButtonAnimator.buttonToggleDuration,
+      y: this.hamburgerButton.isOpen ? HamburgerButtonAnimator.yOffset : 0,
+      rotation: this.hamburgerButton.isOpen ? 45 : 0,
+      transformOrigin: "center",
+    });
+    gsap.to(
+      this.hamburgerButton.middleLine,
+      {
+        duration: HamburgerButtonAnimator.buttonToggleDuration,
+        opacity: this.hamburgerButton.isOpen ? 0 : 1,
+      },
+      "<",
+    );
+    gsap.to(
+      this.hamburgerButton.bottomLine,
+      {
+        duration: HamburgerButtonAnimator.buttonToggleDuration,
+        y: this.hamburgerButton.isOpen ? -HamburgerButtonAnimator.yOffset : 0,
+        rotation: this.hamburgerButton.isOpen ? -45 : 0,
+        transformOrigin: "center",
+      },
+      "<",
+    );
+  }
+}
+
+class HamburgerMenu {
+  constructor(menuInstance, navBar, hamburgerButton) {
+    this.menuInstance = menuInstance;
+    this.navBar = navBar;
+    this.hamburgerButton = hamburgerButton;
+  }
+}
+
+class HamburgerMenuAnimator {
+  constructor(hamburgerMenu) {
+    this.hamburgerMenu = hamburgerMenu;
+  }
+
+  animateHamburgerMenu() {
+    if (this.hamburgerMenu.hamburgerButton.isOpen) {
+      gsap.to(this.hamburgerMenu.menuInstance, {
+        duration: 0.3,
+        yPercent: 150,
+        ease: "expo.out",
+      });
+      gsap.from(this.hamburgerMenu.navBar, {
+        duration: 0.3,
+        y: -50,
+        ease: "power1.inOut",
+      });
+    } else {
+      gsap.to(this.hamburgerMenu.menuInstance, {
+        duration: 0.3,
+        yPercent: -150,
+        ease: "expo.in",
+      });
+    }
+  }
+
+  clearProps() {
+    gsap.set(
+      [
+        this.hamburgerMenu.menuInstance,
+        this.hamburgerMenu.hamburgerButton.buttonInstance,
+      ],
+      { clearProps: "all" },
+    );
+  }
+}
